55359c6c94037b7949b34bb7e941f749e04921ca
rare234@naver.com <128617268+BlessDad@users.noreply.github.com>
2024-05-22 18:27:46 +0900

    이미지 경로 수정 (절대경로)

--- src/main/java/com/example/myspringserver/config/WebConfig.java
+++ src/main/java/com/example/myspringserver/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:src/main/resources/static/uploads/");
+                .addResourceLocations("file:/home/ubuntu/images/");
     }
 }
